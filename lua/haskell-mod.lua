07461aedb05c728bc9532c4c046ba8530236bab4
minco <mail@drchi.co.kr>
2025-11-25 09:43:29 +0900

    chore: api

--- lua/haskell-mod.lua
+++ lua/haskell-mod.lua
@@ -3,21 +3,21 @@ local fn = vim.fn
 
 --- @return table
 local function setup()
-    vim.api.nvim_create_autocmd("BufNewFile", {
+    api.nvim_create_autocmd("BufNewFile", {
         pattern = "*.hs",
         callback = function()
-            local buf = vim.api.nvim_get_current_buf()
-            local filepath = vim.api.nvim_buf_get_name(buf)
-            local root = vim.fn.finddir(".git/..", filepath .. ";")
+            local buf = api.nvim_get_current_buf()
+            local filepath = api.nvim_buf_get_name(buf)
+            local root = fn.finddir(".git/..", filepath .. ";")
             if root == "" then
                 return
             end
             local module_path = filepath:gsub("^" .. root .. "/", "")
             module_path = module_path:gsub("%.hs$", "")
             module_path = module_path:gsub("/", ".")
-            local first_line = vim.api.nvim_buf_get_lines(buf, 0, 1, false)[1]
+            local first_line = api.nvim_buf_get_lines(buf, 0, 1, false)[1]
             if first_line == "" then
-                vim.api.nvim_buf_set_lines(buf, 0, 0, false, { "module " .. module_path .. " where", "" })
+                api.nvim_buf_set_lines(buf, 0, 0, false, { "module " .. module_path .. " where", "" })
             end
         end
     })
